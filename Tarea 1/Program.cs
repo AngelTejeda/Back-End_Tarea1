96f4d31cf92aa13e7dca84e669deafa83ac4afdb
Ángel Tejeda <angel.tejedat@gmail.com>
2021-02-28 16:53:55 -0600

    First Commit

---
+++ Tarea 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Tarea_1.DataAccess;
+
+namespace Tarea_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NorthwindContext dbContext = new NorthwindContext();
+
+            var empleados = dbContext.Employees.Select(s => new
+            {
+                Nombre = s.FirstName,
+                Apellidos = s.LastName,
+                Puesto = s.Title
+            }
+            ).ToList();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
